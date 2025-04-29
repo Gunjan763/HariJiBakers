8d95176b4eb6262c62ea60a482d8f318d5c59ba7
Gunjan <gunjanaggarwal278@gmail.com>
2025-04-29 21:53:57 +0530

    Git Updated

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     tailwindcss(),
   ],
   build: {
-    outDir: 'dist',  // Specify the build output directory
+    outDir: 'build',  // Specify the build output directory
     assetsDir: 'assets',  // Folder for static assets like images, fonts, etc.
     rollupOptions: {
       input: 'index.html', // Main entry file for the app
