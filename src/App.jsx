fce810f715f3aa4fb7cc082aaa49df9d53a1e98a
Gunjan <gunjanaggarwal278@gmail.com>
2025-05-01 22:06:44 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Full pages
 import First from "./Home/first.jsx";
@@ -12,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<First />} />
         <Route path="/home" element={<First />} />
-        <Route path="/services" element={<Second />} />
+        <Route path="/services"element={<Second />} />
         <Route path="/about" element={<Third />} />
         <Route path="/contact" element={<Fourth />} />
       </Routes>
